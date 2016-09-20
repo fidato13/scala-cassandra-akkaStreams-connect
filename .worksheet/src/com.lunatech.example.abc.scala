f9ea8ea78ecea259ad1a2951f44075aac1a85336
fidato <fidato.july13@gmail.com>
2016-09-21 00:00:43 +0200

    removed extras

--- .worksheet/src/com.lunatech.example.abc.scala
+++
@@ -1,5 +0,0 @@
-package com.lunatech.example
-
-object abc {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(90); 
-  val d = List[String]("a","b", "c", "d", null);System.out.println("""d  : List[String] = """ + $show(d ))}
-}
